b3bc15db0a712d953cc593ff5e1abf05f1c9d44a
meena-coder-source <2021wb86878@wilp.bits-pilani.ac.in>
2025-06-15 09:56:35 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.util.logging.Logger;
+
+public class Hello {
+    private static final Logger logger = Logger.getLogger(Hello.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Hello from clean setup!");
+    }
+}
